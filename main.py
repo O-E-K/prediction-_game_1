1d4456a02440d02e265124a43a4c0282f5e23edb
O-E-K <37031490+O-E-K@users.noreply.github.com>
2022-03-06 18:12:44 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 num_attempt = 0
 num_correct = 0
 predict = 0
-current_state = 0
-next_state = 0
+lock_len = 0
+current_lock_state = 0
+next_lock_state = 0
+delta_len = 0
+current_delta = 0
+deltas_found = [0, 0, 0, 0, 0, 0, 0, 0]
 accuracy = (num_correct / num_attempt)
 
-# getting the user input
+def setup():
+    global current_lock_state
+    global delta_len
+    global lock_len
+
+
 def attempt():
     global predict
     global num_attempt
     predict = input("What is the next sequence? ")
     num_attempt += 1
 
-# checking user input for correctness    
+
 def check():
     global predict
-    global next_state
+    global next_lock_state
     global num_correct
-    if predict == next_state:
+    if predict == next_lock_state:
         num_correct += 1
-        #add string letter check
+        deltas_found[current_delta] = 1
+        # add string letter check
     else:
         pass
